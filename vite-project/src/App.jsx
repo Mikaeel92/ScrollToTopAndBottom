af8d50e4ab5cefc2a15e5d3aa1150d80ac10183f
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-12 10:57:55 +0330

    step three, ui

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,7 +6,15 @@ const App = () => {
   const {data} = UseFetch('https://dummyjson.com/products?limit=100')
 
   return (
-    <div>App</div>
+    <div className='flex flex-col gap-2 items-center'>
+    <h1 className='text-2xl font-bold'>Scroll To Top And Bottom Feature</h1>
+    <p className='font-bold text-green-700'>This is the top section</p>
+    <button onClick={handleScrollToBottom} className='bg-gray-700 rounded-md p-2 text-white'>Scroll To Bottom</button>
+
+    <button onClick={handleScrollToTop} className='bg-gray-700 rounded-md p-2 text-white'>Scroll To Top</button>
+    <div ref={}></div>
+    <p className='font-bold text-red-700'>This is the bottom of the page</p>
+    </div>
   )
 }
 
